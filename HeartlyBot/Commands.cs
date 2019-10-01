b8df2259c0578ee7919e0000aec1ac7a0d72583c
Brady Rhora <brady0423@gmail.com>
2019-10-01 15:46:56 -0400

    finished but its stupid

--- HeartlyBot/Commands.cs
+++ HeartlyBot/Commands.cs
@@ -33,12 +33,100 @@ namespace HeartlyBot
             }
             await Context.Channel.SendMessageAsync("", embed: emb.Build());
         }
-
+    
         [Command("ping"), Summary("Pong!")]
-        public async Task COMMAND()
+        public async Task Ping()
         {
             await ReplyAsync("Pong! :heart:");
         }
+
+        [Command("anagram"), Summary("Rearrange text to make... Different text!!!")]
+        public async Task Anagram([Remainder] string text)
+        {
+            text = text.ToLower();
+            char[] allowedChars = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            Dictionary<char, int> charDict = new Dictionary<char, int>();
+
+            foreach (char c in text)
+            {
+                if (allowedChars.Contains(c))
+                {
+                    if (charDict.ContainsKey(c)) charDict[c]++;
+                    else charDict.Add(c, 1);
+                }
+            }
+            
+            string[] words = File.ReadAllLines("commonwords.txt");
+            List<string> possibleWords = new List<string>();
+            foreach(string w in words)
+            {
+                var testDict = new Dictionary<char, int>(charDict);
+                bool hasWord = true;
+                foreach(char c in w)
+                {
+                    if (testDict.ContainsKey(c))
+                    {
+                        testDict[c]--;
+                        if (testDict[c] <= 0) testDict.Remove(c);
+                    }
+                    else { hasWord = false; break; }
+                }
+
+                if (hasWord) possibleWords.Add(w);
+            }
+
+            List<string> anagrams = new List<string>();
+            foreach(string w1 in possibleWords)
+            {
+                foreach(string w2 in possibleWords)
+                {
+                    foreach(string w3 in possibleWords)
+                    {
+                        string anagram = "";
+                        var testDict = new Dictionary<char, int>(charDict);
+                        var allChars = w1 + " " + w2 + " " + w3;
+
+                        string potWord = "";
+                        foreach (char c in allChars)
+                        {
+                            if (c == ' ')
+                            {
+
+                                anagram += potWord + ' ';
+                                potWord = "";
+
+                                if (testDict.Keys.Count() == 0) {
+                                    if (!anagrams.Contains(anagram) || anagram == text)
+                                        anagrams.Add(anagram);
+                                    break;
+                                }
+                            }
+                            else if (testDict.ContainsKey(c))
+                            {
+                                testDict[c]--;
+                                if (testDict[c] <= 0) testDict.Remove(c);
+                                potWord += c;
+                            }
+                            else break;
+                        }
+                    }
+                }
+            }
+
+            int anagramCount = anagrams.Count();
+            int loopCount;
+            if (anagramCount > 10) loopCount = 10;
+            else loopCount = anagramCount;
+
+            string aList = "```\n";
+            for (int i = 0; i < loopCount; i++) aList += anagrams[i] + "\n";
+            aList += "```";
+
+            if (anagramCount > 0) await ReplyAsync(anagramCount + " anagram(s) found: " + aList);
+            else await ReplyAsync("No anagrams found.");
+        }
+        
 
         public static Color GetColor(IUser User)
         {
